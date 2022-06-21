cc99c93e90c9aba620741b7c91f9a655e934c935
Derik McCausland <derikmccausland@gmail.com>
2022-06-21 16:56:36 -0600

    Drill work

---
+++ guess.js
@@ -0,0 +1,13 @@
+let namearr = ['Steven', 'Bryan']
+
+function nameCheck(name) {
+  for (var i = 0; i < arguments.length; i++) {
+    if (i === name) {
+      console.log(`What is up ${name}`)
+    } else {
+      console.log(`What is up ${name}`)
+    }
+  } i++
+}
+
+nameCheck('Steven')
